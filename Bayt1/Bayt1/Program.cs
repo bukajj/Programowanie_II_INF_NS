ec59892e47f9625f637b0b6b746094ce21b55a84
bukajj <42746240+bukajj@users.noreply.github.com>
2019-11-27 21:09:35 +0100

    bayt1-1.0

---
+++ Bayt1/Bayt1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Bayt1
+{
+    public class Program
+    {
+        internal static bool CheckLength(string s1, string s2)
+        {
+            return s1.Length == s2.Length ? true : false;
+        }
+
+        internal static bool CheckLetterCase(string s1, string s2)
+        {
+            return String.Equals(s1.ToLower(), s2.ToLower()) ? true : false;
+        }
+
+        internal static string StringSort(string s)
+        {
+            char[] tab = s.ToCharArray();
+            Array.Sort(tab);
+            return new string(tab);
+        }
+
+        internal static bool CheckOrder(string s1, string s2)
+        {
+            return String.Equals(StringSort(s1), StringSort(s2)) ? true : false;
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj pierwszy łańcuch");
+            string s1 = Console.ReadLine();
+            Console.WriteLine("Podaj drugi łańcuch");
+            string s2 = Console.ReadLine();
+        }
+    }
+}
